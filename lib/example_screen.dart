c49a1b6abbf8ff369af065ea0dba52c046cd7eb6
kinci89 <kinci@naver.com>
2024-07-14 13:26:44 +0900

    기초적인 코드는 완성

--- lib/example_screen.dart
+++ lib/example_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bolg/platformChannel/method_channel_util.dart';
 import 'package:flutter/material.dart';
 
 class ExampleScreen extends StatefulWidget {
@@ -14,13 +15,29 @@ class _ExampleScreenState extends State<ExampleScreen> {
       appBar: AppBar(
         title: const Text('기능예시 리스트'),
       ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('메소드기능'),
-          )
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                MethodChannelUtil.getPlatformVersion();
+              },
+              child: const Text('1.안드로이드 버전 가져오기'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                MethodChannelUtil.showMyToast(msg : '안녕하세요!');
+              },
+              child: const Text('2.토스트메세지기능'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                MethodChannelUtil.wrongMethod();
+              },
+              child: const Text('3.잘못된메소드명호출하기'),
+            )
+          ],
+        ),
       ),
     );
   }
